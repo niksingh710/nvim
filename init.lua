ccb0bdd4b4f49f63d844e1b6bf78e4db58c4b528
niksingh710 <nik.singh710@gmail.com>
2024-02-16 21:06:25 +0530

    Cleaning Master | Backup -> old-1

--- init.lua
+++
@@ -1,8 +0,0 @@
-require("core")
-utils.load.mappings("general")
-
-require("plugins")
-vim.cmd("colorscheme material")
-
--- TODO: Lsp Semantics token solution
--- TODO: Nerog
--- lua/core/init.lua
+++
@@ -1,5 +0,0 @@
----@diagnostic disable-next-line: lowercase-global
-icons = require "core.icons"
-utils = require "core.utils"
-require "core.options"
-require "core.autocmds"
